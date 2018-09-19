6d9aefaa523eff9c47bf29d8d0668f6f15f2dee5
Marcell Monteiro Cruz <0000marcell@gmail.com>
2018-09-19 20:48:12 -0300

    default time to sleep

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
 
+let time = process.argv[2];
+if(!time) time = 36000
+
 exec('open https://archive.org/details/FOTR22chapter', (err) => {
   if(err) console.error(err); 
 });
@@ -9,4 +12,4 @@ setTimeout(function(){
   exec('pmset sleepnow', (err) => {
     if(err) console.error(err);
   });
-}, 1000)
+}, time)
